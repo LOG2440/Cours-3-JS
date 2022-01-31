209b2cea4af482f89159defd2885d66c4dc54e84
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2022-01-31 10:24:09 -0500

    Update team_manager to initial code.

--- team_manager/script.js
+++ team_manager/script.js
@@ -5,32 +5,40 @@ const teams = ["Habs",
 "Penguins"];
 
 
-
+function loadTeams() {
+  const listItems = document.getElementsByClassName("team");
+  for (var i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener("click", (e) => {
+      e.target.textContent = teams[i];
+    });
+  }
+}
 
 /* Solution #1
 Remplacer var par let
 */
 
 // Solution #2
-function loadTeams() {
-  const listItems = document.getElementsByClassName("team");
-  for (var i = 0; i < listItems.length; i++) {
-    (function (newI) {
-      listItems[newI].addEventListener("click", (e) => {
-        e.target.textContent = teams[newI];
-      });
-    })(i);
-  }
-}
+// function loadTeams() {
+//   const listItems = document.getElementsByClassName("team");
+//   for (var i = 0; i < listItems.length; i++) {
+//     (function (newI) {
+//       listItems[newI].addEventListener("click", (e) => {
+//         e.target.textContent = teams[newI];
+//       });
+//     })(i);
+//   }
+// }
+
 
 // Solution #3
 // Variant de #2 avec le mot clé const
-function loadTeams() {
-  const listItems = document.getElementsByClassName("team");
-  for (var i = 0; i < listItems.length; i++) {
-    const newI = i;
-    listItems[newI].addEventListener("click", (e) => {
-      e.target.textContent = teams[newI];
-    });
-  }
-}
+// function loadTeams() {
+//   const listItems = document.getElementsByClassName("team");
+//   for (var i = 0; i < listItems.length; i++) {
+//     const newI = i;
+//     listItems[newI].addEventListener("click", (e) => {
+//       e.target.textContent = teams[newI];
+//     });
+//   }
+// }
